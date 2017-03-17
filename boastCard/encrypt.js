cee9b130a630f8dbc86e52dd25806027cc9c7bf6
gjc9620 <992758355@qq.com>
2017-03-17 11:35:35 +0800

    commit

--- boastCard/encrypt.js
+++ boastCard/encrypt.js
@@ -6,12 +6,8 @@ var crypto=require('crypto');
 var myKey = "gjc1994216"
 
 var cipher = crypto.createCipher('aes-256-cbc',myKey);
-var text = "ok";
+var text = "999990个小时";
 var crypted = cipher.update(text,'utf8','hex');
 crypted += cipher.final('hex');
 console.log('加密后的文本：'+crypted);
 
-var decipher = crypto.createDecipher('aes-256-cbc',myKey);
-var dec = decipher.update(crypted,'hex','utf8');
-dec += decipher.final('utf8');
-console.log('解密的文本：'+dec);
