6a412107b6d77e8c8e2fb64a07b43d7eed85181f
gjc9620 <992758355@qq.com>
2017-10-08 15:30:06 +0800

    electron

---
+++ electron/renderer.js
@@ -0,0 +1,25 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+
+
+
+//
+// const {app, globalShortcut} = require('electron')
+//
+// app.on('ready', () => {
+//   globalShortcut.register('CommandOrControl+X', () => {
+//     console.log('CommandOrControl+X is pressed')
+//   })
+// })
+
+
+const {clipboard }= require('electron');
+
+
+const pasteBtn = document.getElementById('paste-to');
+
+pasteBtn.addEventListener('click', function () {
+  // debugger
+  clipboard.writeText('I am a good guy');
+});
