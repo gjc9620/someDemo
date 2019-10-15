b23be22430dae9cc419b4a3e620e0f882c0d0c8b
gujiacheng <992758355@qq.com>
2019-10-15 21:58:54 +0800

    puppeteer

--- puppeteer/index.js
+++ puppeteer/index.js
@@ -1,29 +1,10 @@
 const puppeteer = require('puppeteer');
 
-function a(){
-  console.log('aa');
-}
-
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto('https://juejin.im');
-  // const texts = page.evaluate(() => document.querySelectorAll('.scrape').map(v=>v.textContent));
-  // console.log(texts);
-  
-  // const textContent = await page.evaluate(() => document.querySelector('p').textContent);
-  // const innerText = await page.evaluate(() => document.querySelector('p').innerText);
   const text = await page.evaluate(() => Array.from(document.querySelectorAll('.title-row > .title'), element => `${element.textContent} ${element.href}`));
-  
-  const bodyHTML =
-  await page.evaluate(
-    () => document.body.innerHTML
-  );
-  
-  // console.log(textContent);
-  console.log(bodyHTML);
   console.log(text);
-  
   await browser.close();
-  
 })();
